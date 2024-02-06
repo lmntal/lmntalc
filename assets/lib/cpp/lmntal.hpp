20d36be01d12a65e7eda5d8caa9d8f1f22e032ce
Qirui WANG <mr.qrwang@gmail.com>
2024-02-07 02:47:25 +0900

    refactor(lib): refined c++ lib

--- assets/lib/cpp/lmntal.hpp
+++ assets/lib/cpp/lmntal.hpp
@@ -3,11 +3,12 @@
 #include <cstdint>
 #include <iostream>
 #include <list>
-#include <map>
+#include <memory_resource>
 #include <queue>
 #include <random>
 #include <ranges>
 #include <sstream>
+#include <stack>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
@@ -33,8 +34,8 @@ struct link {
   ::atom *atom;
   int64_t port;
 
-  [[nodiscard]] bool is_normal() const { return port >= 0; }
-  [[nodiscard]] bool is_hyperlink() const { return port < 0; }
+  [[nodiscard]] auto is_normal() const -> bool { return port >= 0; }
+  [[nodiscard]] auto is_hyperlink() const -> bool { return port < 0; }
   [[nodiscard]] auto get_port() const -> size_t {
     return static_cast<size_t>(port);
   }
@@ -45,9 +46,11 @@ struct link {
     return is_hyperlink() ? reinterpret_cast<hyperlink *>(atom) : nullptr;
   }
   [[nodiscard]] auto is_int() const -> bool;
-  auto get_int() const -> int64_t;
+  [[nodiscard]] auto get_int() const -> int64_t;
+  void set_int(int64_t value);
   [[nodiscard]] auto is_float() const -> bool;
-  auto get_float() const -> double;
+  [[nodiscard]] auto get_float() const -> double;
+  void set_float(double value);
 };
 
 class hyperlink final : public atom {
@@ -64,83 +67,86 @@ private:
 };
 
 class normal_atom final : public atom {
-public:
-  explicit normal_atom(int32_t const label) : label(label) {}
-  normal_atom(int32_t const label, size_t const bond_count)
-      : label(label), bonds(bond_count) {}
+ public:
+  explicit normal_atom(int32_t const label) : label_(label) {}
 
-  [[nodiscard]] auto at(size_t const pos) const -> link { return bonds[pos]; }
+  [[nodiscard]] auto at(size_t const pos) const -> link { return bonds_[pos]; }
   auto link_with(size_t const pos, normal_atom *atom, size_t const atom_pos)
       -> void {
-    bonds[pos].atom = atom;
-    bonds[pos].port = static_cast<int64_t>(atom_pos);
+    bonds_[pos].atom = atom;
+    bonds_[pos].port = static_cast<int64_t>(atom_pos);
   }
   auto link_with(size_t const pos, hyperlink *hlink) -> void {
-    bonds[pos].atom = hlink;
-    bonds[pos].port = -1;
+    bonds_[pos].atom = hlink;
+    bonds_[pos].port = -1;
   }
   auto remove_at(size_t const pos) -> void {
-    auto *atom = bonds[pos].get_normal();
+    auto *atom = bonds_[pos].get_normal();
     atom->set_remove();
-    bonds[pos].atom = nullptr;
+    bonds_[pos].atom = nullptr;
   }
-  [[nodiscard]] auto get_arity() const -> size_t { return bonds.size(); }
+  [[nodiscard]] auto get_arity() const -> size_t { return arity_; }
   [[nodiscard]] auto is_plain() const -> bool {
-    return data.index() == plain_index;
+    return data_.index() == PlainIndex;
   }
   [[nodiscard]] auto is_int() const -> bool {
-    return data.index() == int_index;
+    return data_.index() == IntIndex;
   }
   [[nodiscard]] auto get_int() const -> int64_t {
-    return std::get<int64_t>(data);
+    return std::get<int64_t>(data_);
   }
-  void set_int(int64_t value) { data = value; }
+  void set_int(int64_t value) { data_ = value; }
   [[nodiscard]] auto is_float() const -> bool {
-    return data.index() == float_index;
+    return data_.index() == FloatIndex;
   }
   [[nodiscard]] auto get_float() const -> double {
-    return std::get<double>(data);
+    return std::get<double>(data_);
   }
-  void set_float(double value) { data = value; }
-  void set_remove() { flag = static_cast<atom_flag>(flag | atom_flag::Delete); }
+  void set_float(double value) { data_ = value; }
+  void set_remove() {
+    flag_ = static_cast<atom_flag>(flag_ | atom_flag::Delete);
+  }
   void restore_remove() {
-    flag = static_cast<atom_flag>(flag & ~atom_flag::Delete);
+    flag_ = static_cast<atom_flag>(flag_ & ~atom_flag::Delete);
   }
-  [[nodiscard]] bool is_removed() const { return flag & atom_flag::Delete; }
-  [[nodiscard]] hyperlink *get_hlink_at_port(size_t pos) const;
+  [[nodiscard]] auto is_removed() const -> bool {
+    return (flag_ & atom_flag::Delete) != 0;
+  }
+  [[nodiscard]] auto get_hlink_at_port(size_t pos) const -> hyperlink *;
 
-  [[nodiscard]] std::string to_string() const {
-    switch (data.index()) {
-    case int_index:
-      return ::std::to_string(get_int());
-    case float_index:
-      return ::std::to_string(get_float());
-    default:
-      return std::string{int2str(label)};
+  [[nodiscard]] auto to_string() const -> std::string {
+    switch (data_.index()) {
+      case IntIndex:
+        return ::std::to_string(get_int());
+      case FloatIndex:
+        return ::std::to_string(get_float());
+      default:
+        return std::string{int2str(label_)};
     }
   }
 
-  [[nodiscard]] std::vector<link> const &get_bonds() const { return bonds; }
-  [[nodiscard]] name_t get_label() const { return label; }
+  [[nodiscard]] auto get_label() const -> name_t { return label_; }
 
-private:
+ private:
   friend hyperlink;
   friend atom_store;
 
   using data_type = std::variant<std::monostate, int64_t, double>;
-  constexpr static size_t plain_index = 0;
-  constexpr static size_t int_index = 1;
-  constexpr static size_t float_index = 2;
+  constexpr static size_t PlainIndex = 0;
+  constexpr static size_t IntIndex = 1;
+  constexpr static size_t FloatIndex = 2;
 
-  name_t label{};
-  atom_flag flag{};
-  data_type data{std::monostate{}};
-  std::vector<link> bonds{};
+  name_t label_{};
+  uint32_t arity_;
+  atom_flag flag_{};
+  data_type data_{std::monostate{}};
+  // std::vector<link> bonds{};
+  link bonds_[1];
 
   void clone_to(normal_atom *atom) const {
-    atom->flag = flag;
-    atom->bonds = bonds;
-    atom->data = data;
+    atom->flag_ = flag_;
+    std::copy(bonds_, bonds_ + arity_, atom->bonds_);
+    atom->data_ = data_;
   }
 };
 
@@ -148,20 +154,22 @@ auto link::is_int() const -> bool {
   return is_normal() and get_normal()->is_int();
 }
 auto link::get_int() const -> int64_t { return get_normal()->get_int(); }
+void link::set_int(int64_t value) { get_normal()->set_int(value); }
 auto link::is_float() const -> bool {
   return is_normal() and get_normal()->is_float();
 }
 auto link::get_float() const -> double { return get_normal()->get_float(); }
+void link::set_float(double value) { get_normal()->set_float(value); }
 
 auto hyperlink::add(normal_atom *atom, size_t const pos) -> void {
   atoms.insert(atom);
-  atom->bonds[pos].atom = this;
-  atom->bonds[pos].port = -1;
+  atom->bonds_[pos].atom = this;
+  atom->bonds_[pos].port = -1;
 }
 
 auto hyperlink::remove(normal_atom *atom, size_t const pos) -> void {
   atoms.erase(atom);
-  atom->bonds[pos].atom = nullptr;
+  atom->bonds_[pos].atom = nullptr;
 }
 
 auto get_hlink_at_port(const normal_atom *atom, const size_t pos)
@@ -170,32 +178,46 @@ auto get_hlink_at_port(const normal_atom *atom, const size_t pos)
 }
 
 class atom_store {
-public:
+ public:
   atom_store() = default;
+  atom_store(std::initializer_list<size_t> sizes) {
+    for (auto size : sizes) {
+      atoms_by_arity_[size] = std::vector<normal_atom *>{};
+      atoms_by_arity_[size].reserve(32);
+      remove_queue_[size] = {};
+    }
+  }
 
   auto create_atom(name_t const name, size_t const bond_count)
       -> normal_atom * {
-    if (remove_queue_.contains(bond_count) and
-        !remove_queue_[bond_count].empty()) {
-      auto *a = remove_queue_[bond_count].front();
-      remove_queue_[bond_count].pop();
-      a->label = name;
+    if (auto it = remove_queue_.find(bond_count);
+        it != remove_queue_.end() and !it->second.empty()) {
+      auto &q = it->second;
+      auto *a = q.top();
+      q.pop();
+      a->label_ = name;
       a->restore_remove();
       return a;
     }
-    auto *a = new normal_atom{name, bond_count};
+    // allocate memory for the atom
+    void *mem =
+        pool_.allocate(sizeof(normal_atom) + (bond_count - 1) * sizeof(link));
+    auto *a = new (mem) normal_atom{name};
+    a->arity_ = bond_count;
 
-    if (not atoms_by_arity_.contains(bond_count)) {
-      atoms_by_arity_[bond_count] = std::vector<normal_atom *>{};
+    if (not atoms_by_arity_.contains(bond_count)) [[unlikely]] {
+      auto vec = std::vector<normal_atom *>{};
+      vec.reserve(16);
+      atoms_by_arity_.emplace(bond_count, std::move(vec));
     }
 
-    atoms_by_arity_[bond_count].push_back(a);
+    atoms_by_arity_.at(bond_count).push_back(a);
     return a;
   }
 
   auto clone_atom(normal_atom const *atom, size_t const pos) -> normal_atom * {
-    const auto nr = atom->at(pos).get_normal();
-    auto *a = create_atom(nr->label, nr->get_arity());
+    auto *const nr = atom->at(pos).get_normal();
+    auto *a = create_atom(nr->label_, nr->get_arity());
     nr->clone_to(a);
     return a;
   }
@@ -208,21 +230,16 @@ public:
 
   void remove_atom(normal_atom *atom) {
     atom->set_remove();
-    if (not remove_queue_.contains(atom->bonds.size())) {
-      remove_queue_[atom->bonds.size()] = std::queue<normal_atom *>{};
+    if (not remove_queue_.contains(atom->arity_)) {
+      remove_queue_[atom->arity_] = {};
     }
-    remove_queue_[atom->bonds.size()].push(atom);
+    remove_queue_[atom->arity_].push(atom);
   }
 
-  auto find_atom(name_t const name, int const arity)
-      -> std::vector<normal_atom *> {
-    auto res = std::vector<normal_atom *>{};
-    for (auto *atom : atoms_by_arity_[arity]) {
-      if (atom->label == name and not atom->is_removed()) {
-        res.push_back(atom);
-      }
-    }
-    return res;
+  auto find_atom(name_t const name, int const arity) {
+    return atoms_by_arity_[arity] | std::views::filter([name](auto *atom) {
+             return atom->label_ == name and not atom->is_removed();
+           });
   }
 
   [[nodiscard]] auto dump() const -> std::string {
@@ -239,17 +256,20 @@ public:
     return res.str();
   }
 
-private:
-  std::map<uint32_t, std::vector<normal_atom *>> atoms_by_arity_{};
-  std::unordered_map<uint32_t, std::queue<normal_atom *>> remove_queue_{};
+ private:
+  std::unordered_map<uint32_t, std::vector<normal_atom *>> atoms_by_arity_{};
+  std::unordered_map<uint32_t, std::stack<normal_atom *>> remove_queue_{};
   std::unordered_set<hyperlink *> hyperlinks_{};
+
+  std::pmr::unsynchronized_pool_resource pool_{};
 
   static auto dfs_dump(normal_atom const *cur, std::stringstream &ss,
                        std::unordered_set<atom const *> &visited) -> void {
     visited.insert(cur);
     ss << cur->to_string() << "(";
     size_t count = 0;
-    for (auto const &link : cur->get_bonds()) {
+    for (auto i = 0; i < cur->arity_; ++i) {
+      auto link = cur->at(i);
       if (link.atom != nullptr and !visited.contains(link.atom)) {
         // check if the atom is a hyperlink
         if (const auto *na = link.get_normal()) {
@@ -308,8 +328,7 @@ void unify(normal_atom const *atom1, size_t const index1,
   }
 }
 
-auto find_atom(const name_t name, const int arity)
-    -> std::vector<normal_atom *> {
+auto find_atom(const name_t name, const int arity) {
   return global_atoms.find_atom(name, arity);
 }
 
